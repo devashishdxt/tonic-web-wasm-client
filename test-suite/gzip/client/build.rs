83837ca1c9130a8eecdc0832a654cfd944389a45
Tymoteusz Kubicz <tymoteusz.kubicz@gmail.com>
2025-07-29 16:58:55 +0200

    Explicitly disable `transport` in `build`

--- test-suite/gzip/client/build.rs
+++ test-suite/gzip/client/build.rs
@@ -3,6 +3,7 @@ use std::io;
 fn main() -> io::Result<()> {
     tonic_prost_build::configure()
         .build_server(false)
+        .build_transport(false)
         .build_client(true)
         .compile_protos(&["echo.proto"], &["../proto"])
 }
